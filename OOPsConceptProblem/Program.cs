79146bf7a8f1f7d2aab7bce04444eb18ac39b1bc
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-28 10:34:56 +0530

    Add: Example of inheritance concept

--- OOPsConceptProblem/Program.cs
+++ OOPsConceptProblem/Program.cs
@@ -7,6 +7,13 @@
             //Object of class ObjectClass
             ObjectClass objectClass = new ObjectClass();
             objectClass.ObjectClassMethod();
+            //Inheritance Example
+            Car car = new Car();
+            car.Start();
+            car.Stop();
+            Console.WriteLine(car.carName = "XUV");
+            Console.WriteLine(car.year = 2022);
+            car.Drive();
             Console.ReadKey();
         }
     }
---
+++ OOPsConceptProblem/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsConceptProblem
+{
+    public class Vehicle
+    {
+        public int speed;
+        public void Start()
+        {
+            Console.WriteLine("The vehicle is starting.");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("The vehicle is stopping.");
+        }
+    }
+    public class Car : Vehicle
+    {
+        public string carName;
+        public int year;
+
+        public void Drive()
+        {
+            Console.WriteLine("My car name is: " + carName + " It was started in year: " + year);
+        }
+    }
+}
