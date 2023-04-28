18fdae36c7acd633770553245dea7dd3970db730
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-28 10:13:44 +0530

    Add: Example of class and its object

---
+++ OOPsConceptProblem/ObjectClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsConceptProblem
+{
+    public class ObjectClass
+    {
+        public void ObjectClassMethod()
+        {
+            Console.WriteLine("This is example of class and its object");
+        }
+    }
+}
---
+++ OOPsConceptProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPsConceptProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Object of class ObjectClass
+            ObjectClass objectClass = new ObjectClass();
+            objectClass.ObjectClassMethod();
+            Console.ReadKey();
+        }
+    }
+}
